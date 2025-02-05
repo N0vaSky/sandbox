284970c95ad8fac887d059c253272095b913f3af
Josh <142667832+N0vaSky@users.noreply.github.com>
2025-02-05 16:00:28 -0500

    Update ransom.ps1

--- ransom.ps1
+++ ransom.ps1
@@ -1,3 +1,10 @@
 curl https://raw.githubusercontent.com/N0vaSky/Fibratus-Install/refs/heads/main/Install-Fibratus.ps1 | iex
 
 Write-Output "https://github.com/N0vaSky/Ransomware-Samples/archive/refs/heads/main.zip"
+
+Write-Output "fibratus run "kevt.category = 'file'" --forward"
+
+Write-Output "https://www.sublimetext.com/download_thanks?target=win-x64"
+
+
+

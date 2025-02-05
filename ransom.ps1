83559e3d7d883c19a4ab187449fff1cb413dfab6
Josh <142667832+N0vaSky@users.noreply.github.com>
2025-02-05 15:47:56 -0500

    Update ransom.ps1

--- ransom.ps1
+++ ransom.ps1
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/N0vaSky/Fibratus-Install/refs/heads/main/Install-Fibratus.ps1 | iex
 
-iwr -Uri "https://github.com/N0vaSky/Ransomware-Samples/archive/refs/heads/main.zip" -OutFile "Ransomware-Samples.zip"
+Write-Output "https://github.com/N0vaSky/Ransomware-Samples/archive/refs/heads/main.zip"

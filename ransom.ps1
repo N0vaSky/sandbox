7471ac7c6b96d4b8b6ec13e9171092efd803d000
Josh <142667832+N0vaSky@users.noreply.github.com>
2025-02-05 15:35:35 -0500

    Add files via upload

---
+++ ransom.ps1
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/N0vaSky/Fibratus-Install/refs/heads/main/Install-Fibratus.ps1 | iex
+
+iwr https://github.com/N0vaSky/Ransomware-Samples/archive/refs/heads/main.zip
